22c7d886c046ed8a08ec4bfc5443db885e51ac53
onisim.balan <onisim.balan@smarovial.com>
2023-11-23 09:56:52 +0200

    setup project

---
+++ test_sp/src/test/java/test_sp/test_sp/TestSpApplicationTests.java
@@ -0,0 +1,13 @@
+package test_sp.test_sp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
